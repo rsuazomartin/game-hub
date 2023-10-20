63e274e025d64e80abb11cd153eb847b7ac29384
Ricardo Suazo Martin <ricardo_suazo@hotmail.com>
2023-10-19 22:20:41 -0600

    Build a responsive layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import App from "./section-1/App";
 import "./section-1/index.css";
+import App from "./section-1/App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
