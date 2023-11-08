998bc5293fdc57bac86c45fde6573cf99147b6b6
Ricardo Suazo Martin <ricardo_suazo@hotmail.com>
2023-11-08 11:34:45 -0600

    Fix the issue with chakra menus

--- src/section-1/App.tsx
+++ src/section-1/App.tsx
@@ -1,20 +1,20 @@
 // Section 8- Building a Video Game Discovery App
 
-// Section 27- Sorting Games
+// Lesson 29- Fixing the Issue with Chakra Menus
 
-// >-(4)->.- We came here from our 'SortSelector' component to pass the sort order to our
-// --- query object >-(8)-> First we add the 'sortOrder: string' property to our 'GameQuery' Query object.
-// --- In the next render the 'gameQuery' object will be passed to the 'GameGrid' (inspecting this
-// --- component we see there isn´t need to do anything as it already uses the 'GameQuery' object)
+// There are some errors in the console caused because of the <HStack> component, so >-(1)-> change it for a <Flex>
+// ... wrap the <PlatformSelector> in a <Box> and give it a 'paddingRight={5}' (because Flex can´t have
+// ... padding)
 
-// 5.- But we need to update the 'useGames' hook according to the 'sortOrder' (ordering) parameter which
-// --- has to be passed to our RAWG API >-(5)->
-
-//  >-(6)-> We came here from the 'useGames' hook to pass the current sort order to the
-// --- 'SortSelector' component. Now the 'sortOrder' property gives error because must be added to the Props
-// --- in the 'SortSelector' component, so >-(7)-> go there to make this changes
-
-import { Grid, GridItem, HStack, Show, baseTheme } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  HStack,
+  Show,
+  baseTheme,
+} from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 import GameGrid from "../components/GameGrid";
 import GenreList from "../components/GenreList";
@@ -24,7 +24,6 @@ import PlatformSelector from "../components/PlatformSelector";
 import { Platform } from "../hooks/useGames";
 import SortSelector from "../components/SortSelector";
 
-// >-(4)-> Add sortOrder: string to our 'GameQuery' query object
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
@@ -57,23 +56,23 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main" paddingLeft={2}>
-        <HStack spacing={5} paddingLeft={1} marginBottom={5}>
-          <PlatformSelector
-            selectedPlatform={gameQuery.platform}
-            onSelectPlatform={(platform) =>
-              setGameQuery({ ...gameQuery, platform })
-            }
-          />
-          {/* >-(3)-> change the 'sortOrder' property of the 'gameQuery' query object to re-render 
-          --- on the <SortSelector> component */}
+        {/* >-(1)-> Change 'HStack' for 'Flex', wrap 'PlataformSelector' in 'Box' and give it a paddingRight={5} */}
+        <Flex paddingLeft={1} marginBottom={5}>
+          <Box marginRight={5}>
+            <PlatformSelector
+              selectedPlatform={gameQuery.platform}
+              onSelectPlatform={(platform) =>
+                setGameQuery({ ...gameQuery, platform })
+              }
+            />
+          </Box>
           <SortSelector
-            //  >-(6)-> pass the 'sortOrder' to the SortSelector
             sortOrder={gameQuery.sortOrder}
             onSelectSortOrder={(sortOrder) =>
               setGameQuery({ ...gameQuery, sortOrder })
             }
           />
-        </HStack>
+        </Flex>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
